b0218c1784db5cda8acc788659fc1775d2a1e602
GUILHERME ALBERTO EIRAS <RM74395@fiap.com.br>
2016-08-23 11:07:55 -0300

    Criação do método de inserir cliente

--- src/br/com/fiap/dao/ClienteDAO.java
+++ src/br/com/fiap/dao/ClienteDAO.java
@@ -1,10 +1,13 @@
 package br.com.fiap.dao;
 
+import br.com.fiap.conexao.Conexao;
 import br.com.fiap.modelo.Cliente;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 public class ClienteDAO {
     private Connection conexao;
@@ -13,6 +16,18 @@ public class ClienteDAO {
     ResultSet rs;
 
     public void inserirCliente(Cliente cliente) {
-
+        try {
+            conexao = Conexao.getConnection();
+            sql = "INSERT INTO POO_CLIENTE VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement p = conexao.prepareStatement(sql);
+            p.setString(1, cliente.getNome());
+            p.setString(2, cliente.getEndereco());
+            p.setDate(3, cliente.getNascimento());
+            p.setString(4, cliente.getFone());
+            p.setString(5, cliente.getCaminho());
+            p.execute();
+        } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null, "Erro ao inserir cliente\n"+ex);
+        }
     }
 }
