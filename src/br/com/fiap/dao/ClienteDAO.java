efc10f3128d066ac202864f7edd7433941d9b016
GUILHERME ALBERTO EIRAS <RM74395@fiap.com.br>
2016-08-30 08:50:48 -0300

    Adicionado método pesquisarCliente no ClienteDAO

--- src/br/com/fiap/dao/ClienteDAO.java
+++ src/br/com/fiap/dao/ClienteDAO.java
@@ -4,9 +4,11 @@ import br.com.fiap.conexao.Conexao;
 import br.com.fiap.modelo.Cliente;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 public class ClienteDAO {
@@ -32,6 +34,25 @@ public class ClienteDAO {
     }
     
     public Cliente pesquisarCliente(String nome) {
-        return null;
+        Cliente cliente = null;
+        conexao = Conexao.getConnection();
+        sql = "SELECT * FROM POO_CLIENTE WHERE NOME = ?";
+        String endereco, fone, caminho;
+        Date nascimento;
+        try {
+            p = conexao.prepareStatement(sql);
+            p.setString(1, nome);
+            rs = p.executeQuery();
+            if(rs.next()) {
+                endereco = rs.getString("endereco");
+                nascimento = rs.getDate("nascimento");
+                fone = rs.getString("fone");
+                caminho = rs.getString("caminho");
+                cliente = new Cliente(nome, endereco, nascimento, fone, caminho);
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao pesquisar cliente\n"+ex);
+        }
+        return cliente;
     }
 }
